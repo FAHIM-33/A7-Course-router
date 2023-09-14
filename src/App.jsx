4431ffc7b9554f080503ed4777549c2a18b3d4a5
FAHIM-33 <shahriyerfahim2012@gmail.com>
2023-09-15 01:13:05 +0600

    All functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,40 @@ function App() {
 
   let [course, setCourse] = useState([])
   let [credit, setCredit] = useState(0)
-  let [hour, setHour] = useState(0)
-
+  let [price, setPrice] = useState(0)
 
   const handleCourse = (card) => {
+    let totalTime = card.credit + credit;
+    let doesExist = course.find(obj => obj.id === card.id);
+    if (totalTime > 20) {
+      return alert('Exceeding');
+    }
+    if (doesExist) {
+      return alert('Already exists.')
+    }
     let newArr = [...course, card]
     setCourse(newArr);
+    setPrice(price + card.price)
+    setCredit(credit + card.credit)
 
-
-    
   }
 
 
-  
+
+
+
   return (
     <>
       <h1 className='text-center font-bold text-3xl mb-8'>Course Registration</h1>
       <section className='flex gap-6'>
         <Cards
-        handleCourse={handleCourse}
+          handleCourse={handleCourse}
         ></Cards>
+
         <Sidebar
-        course={course}
+          course={course}
+          credit={credit}
+          price={price}
         ></Sidebar>
       </section>
     </>
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,9 @@
 import pt from 'prop-types'
-const Sidebar = ({ course }) => {
+const Sidebar = ({ course, credit, price }) => {
+    let remaining = 20 - credit;
     return (
-        <div className="w-1/4 border border-red-700 p-6">
-            <h2 className="text-prim text-lg font-bold">Credit Hour Remaining { } hr</h2>
+        <div className="w-1/4 p-6">
+            <h2 className="text-prim text-lg font-bold">Credit Hour Remaining {remaining} hr</h2>
             <div className="w-full h-[1px] bg-gray-300 my-4"></div>
             <h2 className="text-hard text-xl font-bold mb-5">Course Name</h2>
             <ol className='list-decimal list-inside mb-6'>
@@ -14,14 +15,16 @@ const Sidebar = ({ course }) => {
                 }
             </ol>
             <div className="w-full h-[1px] bg-gray-300 my-4"></div>
-            <h3 className='my-4 text-mid font-medium'>Total Credit Hour : {}</h3>
+            <h3 className='my-4 text-mid font-medium'>Total Credit Hour : {credit}</h3>
             <div className="w-full h-[1px] bg-gray-300 my-4"></div>
-            <p className='text-mid font-semibold'>Total Price : {}</p>
+            <p className='text-mid font-semibold'>Total Price : { price }</p>
 
         </div>
     );
 };
 Sidebar.propTypes = {
-    course: pt.array
+    course: pt.array,
+    credit: pt.number,
+    price: pt.number,
 }
 export default Sidebar;
